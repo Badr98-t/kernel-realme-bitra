dabbd3b50ebeb6b7ae7095199a0273e2238c38ac
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-28 12:57:20 -0800

    mm: vmpressure: Don't cache the window size
    Caching the window size can result in delayed or inaccurate pressure
    reports. Since calculating a fresh window size is cheap, do so all the
    time instead of relying on a stale, cached value.
    
    Change-Id: I8070b85fd5aedc4628304a969a81304e2ab0b3fc
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -358,9 +358,6 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned, bool critical,
 
 	if (scanned) {
 		spin_lock(&vmpr->sr_lock);
-		if (!vmpr->scanned)
-			calculate_vmpressure_win();
-
 		vmpr->scanned += scanned;
 		vmpr->reclaimed += reclaimed;
 
@@ -372,7 +369,7 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned, bool critical,
 		reclaimed = vmpr->reclaimed;
 		spin_unlock(&vmpr->sr_lock);
 
-		if (scanned < vmpressure_win)
+		if (!critical && scanned < calculate_vmpressure_win())
 			return;
 	}
 
