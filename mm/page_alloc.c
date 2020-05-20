007219ff614cbd65c956c36635e81b2eb6a80fc5
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-20 09:55:17 -0700

    mm: Don't stop kswapd on a per-node basis when there are no waiters
    The page allocator wakes all kswapds in an allocation context's allowed
    nodemask in the slow path, so it doesn't make sense to have the kswapd-
    waiter count per each NUMA node. Instead, it should be a global counter
    to stop all kswapds when there are no failed allocation requests.
    
    Change-Id: Id4f03d61e17aa5b9f0094766aaf49b3da6f9251c
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -78,6 +78,8 @@
 atomic_long_t kswapd_waiters = ATOMIC_LONG_INIT(0);
 
 
+atomic_long_t kswapd_waiters = ATOMIC_LONG_INIT(0);
+
 /* prevent >1 _updater_ of zone percpu pageset ->high and ->batch fields */
 static DEFINE_MUTEX(pcp_batch_high_lock);
 #define MIN_PERCPU_PAGELIST_FRACTION	(8)
