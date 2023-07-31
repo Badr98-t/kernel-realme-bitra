48f885ad515036477601fb88550f3fbbd8b23ce3
lyinx <mideann46@gmail.com>
2023-07-31 22:26:13 +0800

    kona-perf_defconfig: Configure big, LITTLE, and prime CPU masks
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I8d8e30a3a2c3497cf84d06bf1704616f7f81b31d

--- fs/open.c
+++ fs/open.c
@@ -362,7 +362,6 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
-
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
 
