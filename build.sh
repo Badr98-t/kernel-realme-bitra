d3f374e12843d5ccaf1fdb815cf40fc7a1c97f86
Badr98-t <bt98.bt7@gmail.com>
2024-02-16 21:46:15 +0100

    welcome Zorok and add building script

--- build.sh
+++ build.sh
@@ -1,24 +1,92 @@
 #!/bin/bash
+sudo apt update && sudo -H apt-get install bc python2 ccache binutils-aarch64-linux-gnu cpio
 
-#
-# Clone proton-clang toolchain if needed
-#
+kernel_dir="${PWD}"
+CCACHE=$(command -v ccache)
+objdir="${kernel_dir}/out"
+anykernel=$HOME/anykernel
+builddir="${kernel_dir}/build"
+ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
+kernel_name="Zorok-bitra"
+zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
+TC_DIR=$HOME/tc
+CLANG_DIR=$HOME/tc/clang-r498229b
+export CONFIG_FILE="vendor/sm8250_defconfig"
+export ARCH="arm64"
+export KBUILD_BUILD_HOST=badr
+export KBUILD_BUILD_USER=home
 
-KERNEL_DEFCONFIG=vendor/sm8250_defconfig
-DIR=$PWD
-export ARCH=arm64
-export SUBARCH=arm64
-export CLANG_PATH="~/toolchains/bin"
-export PATH="$CLANG_PATH:$PATH"
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-export KBUILD_BUILD_USER=Amog
-export KBUILD_BUILD_HOST=Us
+export PATH="$CLANG_DIR/bin:$PATH"
 
-echo
-echo "Kernel is going to be built using $KERNEL_DEFCONFIG"
-echo
+if ! [ -d "$CLANG_DIR" ]; then
+    echo "Toolchain not found! Cloning to $CLANG_DIR..."
+    if ! git clone -q --depth=1 --single-branch https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/ -b master $TC_DIR; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+fi
 
-make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 O=out $KERNEL_DEFCONFIG
+# Colors
+NC='\033[0m'
+RED='\033[0;31m'
+LRD='\033[1;31m'
+LGR='\033[1;32m'
 
-make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 O=out -j$(nproc --all)
+make_defconfig()
+{
+    START=$(date +"%s")
+    echo -e ${LGR} "########### Generating Defconfig ############${NC}"
+    make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE} -j$(nproc --all)
+}
+compile()
+{
+    cd ${kernel_dir}
+    echo -e ${LGR} "######### Compiling kernel #########${NC}"
+    make -j$(nproc --all) \
+    O=out \
+    ARCH=${ARCH}\
+    CC="ccache clang" \
+    CLANG_TRIPLE="aarch64-linux-gnu-" \
+    CROSS_COMPILE="aarch64-linux-gnu-" \
+    CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+    LLVM=1 \
+    LLVM_IAS=1
+}
+
+completion()
+{
+    cd ${objdir}
+    COMPILED_IMAGE=arch/arm64/boot/Image
+    COMPILED_DTBO=arch/arm64/boot/dtbo.img
+    if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
+
+        git clone -q https://github.com/Badr98-t/AnyKernel3.git -b master $anykernel
+
+        mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
+
+        cd $anykernel
+        find . -name "*.zip" -type f
+        find . -name "*.zip" -type f -delete
+        zip -r AnyKernel.zip *
+        mv AnyKernel.zip $zip_name
+        mv $anykernel/$zip_name $HOME/$zip_name
+        rm -rf $anykernel
+        END=$(date +"%s")
+        DIFF=$(($END - $START))
+        #curl --upload-file $HOME/$zip_name https://free.keep.sh; echo
+        #rm $HOME/$zip_name
+        echo -e ${LGR} "############################################"
+        echo -e ${LGR} "############# OkThisIsEpic!  ##############"
+        echo -e ${LGR} "############################################${NC}"
+        exit 0
+    else
+        echo -e ${RED} "############################################"
+        echo -e ${RED} "##         This Is Not Epic :'(           ##"
+        echo -e ${RED} "############################################${NC}"
+        exit 1
+    fi
+}
+make_defconfig
+compile
+completion
+cd ${kernel_dir}
