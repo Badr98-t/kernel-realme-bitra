5540efebded8c7f9b81ceac8f1c78de040136758
Badr98-t <bt98.bt7@gmail.com>
2024-05-19 12:01:36 +0200

    Revert "mm: vmpressure: Fix rampant inaccuracies caused by stale data usage"
    This reverts commit cf286622e498ba3ccfca62edccc1c71737c14cf1.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4590,7 +4590,6 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	unsigned int zonelist_iter_cookie;
 	int reserve_flags;
 	bool woke_kswapd = false;
-	bool used_vmpressure = false;
 
 
 	/*
@@ -4631,8 +4630,6 @@ restart:
 			atomic_long_inc(&kswapd_waiters);
 			woke_kswapd = true;
 		}
-		if (!used_vmpressure)
-			used_vmpressure = vmpressure_inc_users(order);
 		wake_all_kswapds(order, gfp_mask, ac);
 	}
 
@@ -4727,8 +4724,6 @@ retry:
 		goto nopage;
 
 	/* Try direct reclaim and then allocating */
-	if (!used_vmpressure)
-		used_vmpressure = vmpressure_inc_users(order);
 	page = __alloc_pages_direct_reclaim(gfp_mask, order, alloc_flags, ac,
 							&did_some_progress);
 	if (page)
@@ -4853,8 +4848,6 @@ fail:
 got_pg:
 	if (woke_kswapd)
 		atomic_long_dec(&kswapd_waiters);
-	if (used_vmpressure)
-		vmpressure_dec_users();
 	if (!page)
 		warn_alloc(gfp_mask, ac->nodemask,
 				"page allocation failure: order:%u", order);
