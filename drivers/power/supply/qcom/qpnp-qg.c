846b0d83def84e205cbf525d3a0db9913f6c117d
VEN0M98 <bt98.bt7@gmail.com>
2024-08-05 11:23:53 +0000

    Revert "power: fg-alg: fix wrong logic in maximum limit"
    This reverts commit 945f9c06f117a02992a91ac2ca7440c86230dc4a.

--- drivers/power/supply/qcom/qpnp-qg.c
+++ drivers/power/supply/qcom/qpnp-qg.c
@@ -1253,9 +1253,7 @@ static void process_udata_work(struct work_struct *work)
 		} else if (input_present && cc_soc_delta > MAX_CC_SOC_DELTA) {
 			pr_info("cc_soc %d exceeds FULL, calibrate qg_soc\n",
 				chip->udata.param[QG_CC_SOC].data);
-			rc = qg_trigger_good_ocv(chip);
-			if (rc == 0)
-				chip->cl->cl_skip = true;
+			qg_trigger_good_ocv(chip);
 		} else {
 			chip->cc_soc = chip->udata.param[QG_CC_SOC].data;
 		}
